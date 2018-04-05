671a984d942bf7263ad345538dcaf8dbbb4ffce5
Harshini N <radhaharshini@gmail.com>
2018-04-05 02:52:32 +0100

    Grid view added

---
+++ GridView/app/src/main/java/ImageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.harshinin.gridview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.GridView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    // Constructor
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.i1, R.drawable.i2,
+            R.drawable.i3, R.drawable.i4,
+            R.drawable.i5, R.drawable.i6,
+            R.drawable.i7, R.drawable.i8
+    };
+}
+
