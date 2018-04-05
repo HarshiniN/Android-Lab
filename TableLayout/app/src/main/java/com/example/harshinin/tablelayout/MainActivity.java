38de1e9a1037cb8251006d3356a58969b677072b
Harshini N <radhaharshini@gmail.com>
2018-04-05 06:08:09 +0100

    Table layout added

---
+++ TableLayout/app/src/main/java/com/example/harshinin/tablelayout/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.harshinin.tablelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
